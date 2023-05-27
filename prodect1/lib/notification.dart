24d5d26ee59da1a8ea7eba1ce71bd27f411de1df
ysy56 <ysy561356@naver.com>
2023-05-27 16:23:33 +0900

    feat : 푸시 알림

--- prodect1/lib/notification.dart
+++ prodect1/lib/notification.dart
@@ -5,12 +5,15 @@ import 'package:timezone/timezone.dart' as tz;
 class FlutterLocalNotification {
   FlutterLocalNotification._();
 
-  static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
 
   static init() async {
-    AndroidInitializationSettings androidInitializationSettings = const AndroidInitializationSettings('mipmap/ic_launcher');
+    AndroidInitializationSettings androidInitializationSettings =
+    const AndroidInitializationSettings('mipmap/ic_launcher');
 
-    DarwinInitializationSettings iosInitializationSettings = const DarwinInitializationSettings(
+    DarwinInitializationSettings iosInitializationSettings =
+    const DarwinInitializationSettings(
       requestAlertPermission: false,
       requestBadgePermission: false,
       requestSoundPermission: false,
@@ -25,34 +28,66 @@ class FlutterLocalNotification {
   }
 
   static requestNotificationPermission() {
-    flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<IOSFlutterLocalNotificationsPlugin>()?.requestPermissions(
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+        IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
       alert: true,
       badge: true,
       sound: true,
     );
   }
 
-  static Future<void> showNotification() async {
+  static Future<void> scheduleNotification() async {
+    tz.initializeTimeZones(); // TimeZone Database 초기화
+    tz.setLocalLocation(tz.getLocation('Asia/Seoul')); // TimeZone 설정
+
+    // // 현재 시간
+    // tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    // // 알림이 오늘 오후 4시 15분에 처음 예약되도록 설정
+    // tz.TZDateTime scheduledDate = tz.TZDateTime(
+    //   tz.local,
+    //   now.year,
+    //   now.month,
+    //   now.day,
+    //   16,
+    //   15,
+    // );
+
+    // 특정 시간 설정 (예: 오후 4시 30분)
+    tz.TZDateTime specificTime = tz.TZDateTime(tz.local, DateTime.now().year, DateTime.now().month, DateTime.now().day, 16, 30);
+
+    // 특정 시간으로부터 10분 전 계산
+    tz.TZDateTime scheduledDate = specificTime.subtract(Duration(minutes: 10));
+
+
+    // 알림 시간 설정
+    // tz.TZDateTime scheduledDate = tz.TZDateTime.now(tz.local).add(Duration(minutes: 2)); // 예: 오후 4시
+
     const AndroidNotificationDetails androidNotificationDetails =
-        AndroidNotificationDetails('channelId', 'channelName',
-            channelDescription: 'channel description',
-            importance: Importance.max,
-            priority: Priority.max,
-            showWhen: false);
+    AndroidNotificationDetails(
+      'channelId',
+      'channelName',
+      channelDescription: 'channel description',
+      importance: Importance.max,
+      priority: Priority.max,
+      showWhen: false,
+    );
 
     const NotificationDetails notificationDetails = NotificationDetails(
       android: androidNotificationDetails,
-      iOS: DarwinNotificationDetails(badgeNumber: 1));
+      iOS: DarwinNotificationDetails(badgeNumber: 1),
+    );
 
-    await flutterLocalNotificationsPlugin.zonedSchedule(1, 'test title', 'test body', _timeZoneSetting(), notificationDetails, uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime, androidAllowWhileIdle: true,);
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      1,
+      '문어의 꿈', // 알림 제목
+      '잠들 시간이에요~ // > 0 < //', // 알림 내용
+      scheduledDate,
+      notificationDetails,
+      uiLocalNotificationDateInterpretation:
+      UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+    );
   }
-}
-
-// 알람 시간 세팅
-tz.TZDateTime _timeZoneSetting(){
-  tz.initializeTimeZones(); // TimeZone Database 초기화
-  tz.setLocalLocation(tz.getLocation('Asia/Seoul')); // TimeZone 설정
-  final now = tz.TZDateTime.now(tz.local);
-  var scheculedDate = tz.TZDateTime(tz.local, now.year, now.month, now.day, 17, 39); // 알람 시간
-  return scheculedDate;
 }
